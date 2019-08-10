c6d65a4f483453fb733351a62ac3001d15b0b0ba
Louis Raymond <blackccxex@gmail.com>
2019-08-10 14:38:36 +0700

    Edited SkaiaCraft script for the latest version

--- Gaming/Games/SkaiaCraft.sh
+++ Gaming/Games/SkaiaCraft.sh
@@ -4,8 +4,8 @@ testValue=$(systemctl --user is-active gamemoded)
 
 if [[ $testValue = 'active' ]]; then
 	notify-send -t 10000 "Using GameMode for SkaiaCraft" "\nSkaiaCraft will use several optimization calls by system" -i "/home/ccxex29/.local/share/icons/vanilla.png"
-	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.0.jar"
+	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
 else
-	java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.0.jar"
+	java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
 fi	
 	
