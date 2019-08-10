52b2207295f69daa637dd48f2d507b36706df400
Louis Raymond <blackccxex@gmail.com>
2019-08-10 14:48:14 +0700

    Changed SkaiaCraft and ATLauncher home to home variable

--- Gaming/Games/ATLauncher.sh
+++ Gaming/Games/ATLauncher.sh
@@ -4,7 +4,7 @@ testValue=$(systemctl --user is-active gamemoded)
 
 if [[ $testValue = 'active' ]]; then
 	notify-send -t 10000 "Using GameMode for ATLauncher" "\nATLauncher will use several optimization calls by system" -i "/home/ccxex29/.local/share/icons/modpack.png"
-	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so "/home/ccxex29/Other Games/ATLauncher/ATLauncher_64bit"
+	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so "$HOME/Other Games/ATLauncher/ATLauncher_64bit"
 else
-	"/home/ccxex29/Other Games/ATLauncher/ATLauncher_64bit"
+	"$HOME/Other Games/ATLauncher/ATLauncher_64bit"
 fi
--- Gaming/Games/SkaiaCraft.sh
+++ Gaming/Games/SkaiaCraft.sh
@@ -4,8 +4,8 @@ testValue=$(systemctl --user is-active gamemoded)
 
 if [[ $testValue = 'active' ]]; then
 	notify-send -t 10000 "Using GameMode for SkaiaCraft" "\nSkaiaCraft will use several optimization calls by system" -i "/home/ccxex29/.local/share/icons/vanilla.png"
-	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
+	LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so java -jar "$HOME/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
 else
-	java -jar "/home/ccxex29/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
+	java -jar "$HOME/Other Games/SkaiaLauncherMC/SkaiaCraft Launcher v2.1.jar"
 fi	
 	
