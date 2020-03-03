e27ef7738d92fb571040bcf021ad99d79dad926b
Louis Raymond <blackccxex@gmail.com>
2020-03-03 22:51:01 +0700

    Revert last compositor switch method Add matcha mode for LightDark switcher Minor touchpad settings tidy up Add confirmation to switch WM

--- boot-libinput.sh
+++ boot-libinput.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ### Simple Touchpad Config ###
-#xinput set-prop "ETPS/2 Elantech Touchpad" 304 1000
-xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Locked Drags" 1
-#xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Disable While Typing Enabled" 1
-xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Locked Drags Timeout" 1000
-xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Two-Finger Scrolling" 1 1
-xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Palm Detection" 1
-#xinput set-prop "ETPS/2 Elantech Touchpad" 300 1 1
+##xinput set-prop "ETPS/2 Elantech Touchpad" 304 1000
+#xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Locked Drags" 1
+##xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Disable While Typing Enabled" 1
+#xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Locked Drags Timeout" 1000
+#xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Two-Finger Scrolling" 1 1
+#xinput set-prop "ETPS/2 Elantech Touchpad" "Synaptics Palm Detection" 1
+##xinput set-prop "ETPS/2 Elantech Touchpad" 300 1 1
 
 ### Advanced Touchpad Config ###
 xinput set-prop "ELAN0504:01 04F3:3091 Touchpad" "Synaptics Locked Drags" 1
--- switch-wm-xfce4.sh
+++ switch-wm-xfce4.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+uInput=-1
+
 if [[ $(pgrep awesome) ]]; then
-	kill -9 $(pgrep awesome) $(pgrep compton)
-	xfwm4 --replace --display=:0 &
-	xfce4-panel &
-	notify-send -t 6000 "Window Manager Switch" "\nWindow Manager has been switched to XFWM4!"
+	read -p "Are you sure you want to switch to XFWM4? (Y/n)" uInput
+	uInput=$(echo "$uInput" | awk '{print tolower($0)}')
+	if [[ $uInput = '' ]] || [[ $uInput = 'y' ]] || [[ $uInput = 'yes' ]]; then
+		kill -9 $(pgrep awesome) $(pgrep compton)
+		xfwm4 --replace --display=:0 &
+		xfce4-panel &
+		notify-send -t 6000 "Window Manager Switch" "\nWindow Manager has been switched to XFWM4!"
+	fi
 else
-	kill -9 $(pgrep xfwm4)
-	awesome --replace &
-	compton &
-	killall xfce4-panel &	
-	notify-send -t 6000 "Window Manager Switch" "\nWindow Manager has been switched to AwesomeWM!"
+	read -p "Are you sure you want to switch to AwesomeWM? (Y/n)" uInput
+	uInput=$(echo "$uInput" | awk '{print tolower($0)}')
+	if [[ $uInput = '' ]] || [[ $uInput = 'y' ]] || [[ $uInput = 'yes' ]]; then
+		kill -9 $(pgrep xfwm4)
+		awesome --replace &
+		compton &
+		killall xfce4-panel &	
+		notify-send -t 6000 "Window Manager Switch" "\nWindow Manager has been switched to AwesomeWM!"
+	fi
 fi
 
