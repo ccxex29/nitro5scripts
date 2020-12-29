6e796c2a424ca28f332e8c6d40c06c08e8d7d2df
ccxex29 <blackccxex@gmail.com>
2020-12-30 02:06:58 +0700

    Re-add VirtualAudio Manager
     - The VirtualAudio Manager is able to both enable and disable the
     virtual audio script
    
     - Disabler is no longer needed and considered dangerous to be used.
    
     - Emergency unloading scripts are automatically generated in
     /tmp/virtualaudioscript and can be executed by the user as an
     alternative to the given script
    
     - User is now given the simple unloading script

--- VirtualAudio/virtual-audio-mapping-manager.sh
+++ VirtualAudio/virtual-audio-mapping-manager.sh
@@ -23,7 +23,7 @@ moduleNumbers+=($(pactl load-module module-remap-source master="$bridgeSinkName.
 # Just in case of script failure, output array!
 indexCount=${#moduleNumbers[@]}
 maxIndex=$((${indexCount} - 1))
-echo "Max: ${moduleNumbers[$maxIndex]}"
+#echo "Max: ${moduleNumbers[$maxIndex]}"
 
 printf "Module numbers: "
 printf "("
@@ -33,7 +33,30 @@ do
     [[ $x != ${moduleNumbers[$maxIndex]} ]] && printf " "
 done
 printf ")"
-printf "\n"
+printf "\n\n"
+printf "%s\n" "In case you forgot to remove the modules, check /tmp/virtualaudioscript or run this script:"
+
+for ((i=$maxIndex; i>=0; i--))
+do
+	[[ $i == $maxIndex ]] && flipArray="${moduleNumbers[$i]}" || flipArray="$flipArray ${moduleNumbers[$i]}"
+done
+emergencyScriptPath=/tmp/virtualaudioscript
+emergencyScriptFile=$emergencyScriptPath/$(date +%d%m%Y_%s%N)
+emergencyUnloader="for x in $flipArray; do pactl unload-module \$x; done" 
+[[ ! -d /tmp/virtualaudioscript ]] && mkdir -p $emergencyScriptPath
+echo "$emergencyUnloader" > $emergencyScriptFile
+chmod ug+x $emergencyScriptFile
+printf "$emergencyUnloader\n\n"
+# Debugging End
+
+# A Little Message for Users Whom Have No Idea What To Do
+printf "INSTRUCTIONS\n"
+printf "%s\n" "------------" 
+printf "ABOUT: This program (or script) creates pulseaudio virtual null sinks for your programs to play nice with your microphone input (currently only supports one mic source)\n"
+printf "TO USE THIS:
+1. Open your pulseaudio manager (such as pavucontrol)
+2. Look at the applications section and select ApplicationVirtualSink on your media (such as firefox, steam game
+3. Finally open the recordings section and select ConferenceVirtualSource on your recording application (such as discord, zoom, teams)\n\n"
 
 # Wait For User Input
 uinp=''
@@ -58,7 +81,7 @@ do
     __input=""
     if [[ $unloadInp == 'n' ]]; then
         while [[ $__input != "y" && $__input != "n" ]]
-	do
+	    do
 		read -p "Unloading $iNumber remove? (Y/n) " __input
             __input=$(echo $__input | tr '[:upper:]' '[:lower:]')
             [[ $__input == '' ]] && __input="y"
